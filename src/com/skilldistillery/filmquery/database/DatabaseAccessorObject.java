5328348282cf39c29b30b7c5b27266b6e1ca4f98
aveigel <alexaveigel@yahoo.com>
2019-04-26 18:42:06 -0600

    update DOA methods

--- src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -32,17 +32,18 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 			stmt.setInt(1, actorId);
 			ResultSet rs = stmt.executeQuery();
 			if (rs.next()) {
-				actor = new Actor(); 
+				actor = new Actor();
 				actor.setId(rs.getInt("id"));
 				actor.setfName(rs.getString("first_name"));
 				actor.setlName(rs.getString("last_name"));
-//				actor.setFilms(findFilmsByActorId(actorId)); // An Actor has Films
+				actor.setFilms(findFilmsByActorId(actorId)); // An Actor has Films
 			}
 			rs.close();
 			stmt.close();
 			conn.close();
 		} catch (SQLException e) {
-			e.printStackTrace();
+			System.out.println("No such actor id, please try again.");
+			return null;
 		}
 		return actor;
 	}
@@ -50,11 +51,40 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 	@Override
 	public List<Actor> findActorsByFilmId(int filmId) {
 		// TODO Auto-generated method stub
-		return null;
+		List<Actor> actors = null;
+		try {
+			Connection conn = DriverManager.getConnection(url, user, pwd);
+			String sql = "SELECT id, first_name, last_name FROM actor WHERE id = ?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, filmId);
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				if (actors == null) {
+					actors = new ArrayList<>();
+				}
+
+				Actor actor = new Actor();
+
+				actor.setId(rs.getInt("id"));
+				actor.setfName(rs.getString("first_name"));
+				actor.setlName(rs.getString("last_name"));
+				actors.add(actor);
+
+			}
+			rs.close();
+			stmt.close();
+			conn.close();
+			if (actors == null) {
+				System.out.println("No such actor id, please try again.");
+			}
+		} catch (SQLException e) {
+			return null;
+		}
+		return actors;
 	}
 
 	public List<Film> findFilmsByActorId(int actorId) {
-		List<Film> films = new ArrayList<>();
+		List<Film> films = null;
 		try {
 			Connection conn = DriverManager.getConnection(url, user, pwd);
 			String sql = "SELECT id, title, description, release_year, language_id, rental_duration, ";
@@ -64,6 +94,10 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 			stmt.setInt(1, actorId);
 			ResultSet rs = stmt.executeQuery();
 			while (rs.next()) {
+				if (films == null) {
+					films = new ArrayList<>();
+				}
+
 				int filmId = rs.getInt("id");
 				String title = rs.getString("title");
 				String desc = rs.getString("description");
@@ -82,8 +116,10 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 			rs.close();
 			stmt.close();
 			conn.close();
+			if (films == null) {
+				System.out.println("No such film id, please try again.");
+			}
 		} catch (SQLException e) {
-			e.printStackTrace();
 		}
 		return films;
 	}
