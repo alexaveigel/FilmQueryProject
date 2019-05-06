5f7653bc2d528265c50d99cff1cbbf362ab4a153
aveigel <alexaveigel@yahoo.com>
2019-05-06 06:50:28 -0600

    final commit

--- src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -39,6 +39,7 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 			}
 			conn.close();
 			if (film == null) {
+				System.out.println("No such film id, please try again.");
 				return film;
 			}
 			return film;
@@ -81,7 +82,7 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 			if (films.size() > 0) {
 				return films;
 			} else {
-
+				System.out.println("No match was found, please try again.");
 				return films;
 			}
 
